fae408cffc87600074c7ae2efd00e2631bda7373
unknown <M07C721@WPNLL0019144.europe.intranet>
2015-02-23 10:09:45 +0100

    Test Change

--- src/test.js
+++ src/test.js
@@ -3,6 +3,7 @@ var git  = require('gift');
 
 repo = git("C:\\Projects\\Github\\git-sync\\.git");
 
+
 repo.commits (function(err, commits){
 	if (err) console.error(err);
 	else console.log(commits);
